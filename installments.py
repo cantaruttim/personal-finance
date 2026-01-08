ee309bd3d69483d3a7601f7f40384e9a3b0d8402
Matheus Cantarutti <matheuscantarutti@Matheuss-MacBook-Air.local>
2026-01-08 20:26:14 -0300

    total_expend_on_month

--- installments.py
+++ installments.py
@@ -8,4 +8,16 @@ installment = pd.read_excel(
     sheet_name=SHEET_NAME
 )
 
+# print(installment.info())
+
+def total_expend_on_month(df):
+    df['total_expend_on_month'] = (
+        df
+            .groupby('yearmonth', as_index=False)
+            ['value']
+            .transform('sum')
+        )
+    return df
+installment = total_expend_on_month(installment)
+
 print(installment)
