751588a21e38b3cd445eede50dce84b8e1482a5e
cantaruttim <cantaruttim@outlook.com>
2026-02-26 14:52:42 -0300

    filtrando ultima particao

--- installments.py
+++ installments.py
@@ -1,6 +1,14 @@
 import pandas as pd
 from config.config import FILE_PATH_INSTALL
+from util.util import (
+    ajuste_padrao_anomes, 
+    filtrar_mes_mais_recente
+)
 
 install = pd.read_excel(FILE_PATH_INSTALL , 'installments')
+install = ajuste_padrao_anomes(install, 'ANOMES')
 
-print(install.info())
+
+rec = filtrar_mes_mais_recente(install)
+print(rec)
+# print(install)
--- util/util.py
+++ util/util.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
+'''
+ "===================="
+ "===FUNÇÕES GERAIS==="
+ "====================="
+'''
+
 def ajuste_padrao_anomes(df, coluna: str):
     ''''
         Ajusta o padrão do ANOMES para MMYYYY
@@ -7,10 +13,12 @@ def ajuste_padrao_anomes(df, coluna: str):
     df[f'{coluna}'] = df[f'{coluna}'].astype(str).str.zfill(6)
     return df
 
+
 def fillna_zero(df):
     '''Substitui os casos NaN -> 0'''
     df = df.fillna(0)
     return df
+
 
 def gasto_total_mensal(df, value_discont, categorical, numerical):
     ''''
@@ -18,6 +26,7 @@ def gasto_total_mensal(df, value_discont, categorical, numerical):
     '''
     df['GASTO_MENSAL'] = (df.groupby(categorical)[numerical].transform('sum') - value_discont)
     return df
+
 
 def gasto_total_consolidado(df):
     ''''
@@ -27,12 +36,42 @@ def gasto_total_consolidado(df):
     df['PERC_VARIACAO'] = round((df['GASTO_MENSAL'] - df['GASTO_MENSAL'].shift(1)) / df['GASTO_MENSAL'], 6) * 100
     return df
 
-def select_columns(df, columns : list):
-    '''
-        Seleciona uma lista de colunas de maneira dinâmica
-    '''
+    
 def select_columns(df, columns: list):
     """
     Seleciona uma lista de colunas de maneira dinâmica
     """
     return df[columns].drop_duplicates()
+
+'''
+ "============================="
+ "===FUNÇÕES PARA PARCELADOS==="
+ "============================="
+'''
+
+def filtrar_mes_mais_recente(df, coluna_anomes='ANOMES'):
+    """
+    Retorna apenas as linhas do mês mais recente com base na coluna ANOMES (formato MMYYYY).
+    
+    Parâmetros:
+        df (pd.DataFrame): DataFrame original
+        coluna_anomes (str): Nome da coluna que contém MMYYYY
+    
+    Retorna:
+        pd.DataFrame: DataFrame filtrado apenas com o mês mais recente
+    """
+    
+    # Converte temporariamente para datetime (sem criar coluna no df)
+    datas = pd.to_datetime(
+        df[coluna_anomes].astype(str),
+        format='%m%Y',
+        errors='coerce'
+    )
+    
+    # Identifica a data mais recente
+    data_max = datas.max()
+    
+    # Filtra apenas o mês mais recente
+    df_filtrado = df[datas == data_max].copy()
+    
+    return df_filtrado
