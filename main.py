1b0ac7956cfdbf29c8d5033870eb5002137ea91e
cantaruttim <cantaruttim@outlook.com>
2026-02-25 21:49:49 -0300

    updated

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pandas as pd
 from config.config import FILE_PATH
-from util.util import ajuste_padrao_anomes, gasto_total_mensal
+from util.util import (
+    ajuste_padrao_anomes, 
+    gasto_total_mensal, 
+    gasto_total_consolidado,
+    fillna_zero
+)
 
 gastos = pd.read_excel(FILE_PATH, sheet_name='gastos')
 gastos = ajuste_padrao_anomes(gastos, 'ANOMES')
 gastos = gasto_total_mensal(gastos, 2050, 'ANOMES', 'VALUE')
 
-print(gastos)
+df = fillna_zero(gasto_total_consolidado(gastos))
+
+
+
+print(df)
--- util/util.py
+++ util/util.py
@@ -1,11 +1,24 @@
 import pandas as pd
 
-value_discont = 2050
-
 def ajuste_padrao_anomes(df, coluna: str):
     df[f'{coluna}'] = df[f'{coluna}'].astype(str).str.zfill(6)
     return df
 
+def fillna_zero(df):
+    df = df.fillna(0)
+    return df
+
 def gasto_total_mensal(df, value_discont, categorical, numerical):
+    ''''
+        Função que consolida por anomes a variação do gasto mensal.
+    '''
     df['GASTO_MENSAL'] = (df.groupby(categorical)[numerical].transform('sum') - value_discont)
+    return df
+
+def gasto_total_consolidado(df):
+    ''''
+        Função que consolida por anomes a variação percentual do mês atual com o mês anterior.
+    '''
+    df = df[['ANOMES', 'GASTO_MENSAL']].drop_duplicates()
+    df['PERC_VARIACAO'] = round((df['GASTO_MENSAL'] - df['GASTO_MENSAL'].shift(1)) / df['GASTO_MENSAL'], 6) * 100
     return df
