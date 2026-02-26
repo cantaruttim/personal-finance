08807cc49055d337ab6125c393c2b536fcb57c34
cantaruttim <cantaruttim@outlook.com>
2026-02-25 22:18:37 -0300

    salvando arquivo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from config.config import FILE_PATH
+from config.config import FILE_PATH, FILE_PATH_OUTPUT
 from util.util import (
     ajuste_padrao_anomes, 
     gasto_total_consolidado,
@@ -14,4 +14,11 @@ df = fillna_zero(gasto_total_consolidado(gastos))
 
 gastos = gastos.drop(columns=['GASTO_MENSAL'])
 gastos = gastos.merge(df, on="ANOMES", how="left")
-print(gastos)
+
+
+print("Salvando arquivo consolidado ... ")
+try:
+    gastos.to_excel(f"{FILE_PATH_OUTPUT}finance_report_consolidated.xlsx", index=False)
+    print("Documento salvo com sucesso!!!")
+except:
+    print(ValueError)
