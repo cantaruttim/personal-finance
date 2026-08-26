67e35c560ddb981461891117f7b6bbc76bc0d1e2
cantaruttim <cantaruttim@outlook.com>
2026-08-26 20:11:12 -0300

    added project ms-ingestion

---
+++ ms-ingestion-data/src/main/java/ms_ingestion_data/msIngestionData/MsIngestionDataApplication.java
@@ -0,0 +1,13 @@
+package ms_ingestion_data.msIngestionData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsIngestionDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsIngestionDataApplication.class, args);
+	}
+
+}
---
+++ ms-ingestion-data/src/test/java/ms_ingestion_data/msIngestionData/MsIngestionDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ms_ingestion_data.msIngestionData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsIngestionDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
